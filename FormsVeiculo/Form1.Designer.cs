96ebba229271fec20646d56af334f5c1ba41d408
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-05-11 13:41:05 +0100

    Inserir imagem do carro no forms veiculo

--- FormsVeiculo/Form1.Designer.cs
+++ FormsVeiculo/Form1.Designer.cs
@@ -29,7 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Linen;
+            groupBox1.Location = new Point(32, 29);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(215, 339);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(345, 145);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(312, 223);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -37,11 +61,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(669, 390);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Veículo";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
     }
 }
